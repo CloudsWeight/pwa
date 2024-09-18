9536fcef464c84b31883f514dbf55d46a2d66757
CloudsWeight <CloudsWeight@users.noreply.github.com>
2024-09-18 14:10:33 -0700

    Create accts.py

---
+++ port/assets/accts.py
@@ -0,0 +1,7 @@
+accts = [ # Account Ids, use the beginning '/' to make the URL correct if adding more'
+					'/001-001-0101010-005', 
+					'/001-001-0101010-004', 
+					'/001-001-0101010-003',
+					'/001-001-0101010-001',
+					]			
+# all above accounts above are fictitious, replace with your own
